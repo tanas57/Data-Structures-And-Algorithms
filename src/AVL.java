814ec87a2ff531dd687d6696d540063896034e0b
Tayyip MUSLU <muhammet.muslu@ceng.deu.edu.tr>
2017-01-18 21:11:47 +0300

    AVL-insert and print

--- src/AVL.java
+++ src/AVL.java
@@ -1,5 +1,81 @@
-
-
 public class AVL {
+	private Node root;
+	
+	private class Node
+	{
+		private Node parent;
+		private Node left;
+		private Node right;
+		private int balance;
+		private int data;
+		
+		Node(int key)
+		{
+			parent = left = right = null;
+			data = key;
+			balance = 0;
+		}
+	}
 
+	public void insert(int key)
+	{
+		Node n = new Node(key); // yeni node oluþturduk
+		insert(root,n); // recursive fonksiyona gidiyoruz.
+	}
+	
+	private void insert(Node r, Node n)
+	{
+		if(r == null) // kök boþ ise elemaný kök yapýyoruz.
+		{
+			root = n;
+		}
+		else{
+			// kök boþ deðilse, eðer kökten küçükse sol'a, deðilse sað çocuga ekleyeceðiz
+			if(r.data > n.data) // sol
+			{
+				if(r.left == null) // boþ olana kadar recursive olarak geziyoruz.
+				{  // çocuklar arasý baðlantý yapýldý
+					n.parent = r;
+					r.left = n;
+				}
+				else insert(r.left,n);
+			}
+			else{ // sað
+				if(r.right == null)
+				{   // çocuklar arasý baðlantý yapýldý
+					n.parent = r;
+					r.right = n;
+				}
+				else insert(r.right,n);
+			}
+		}
+		// balance kontrolü
+		balance(n);
+	}
+
+	private void balance(Node n)
+	{
+		n.balance = balance2(n);
+	}
+	
+	private int balance2(Node n)
+	{
+		if(n!=null) return (balance2(n.left) - balance2(n.right));
+		else return 0;
+	}
+	
+	public void print()
+	{
+		printInOrder(root);
+	}
+	
+	private void printInOrder(Node r)
+	{
+		if(r != null)
+		{
+			printInOrder(r.left);
+			System.out.print(r.data + "(" + r.balance +") ");
+			printInOrder(r.right);
+		}
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,12 @@ public class Main {
 
 	public static void main(String [] args)
 	{
-		int[] nums = {5,3,2,3,2,1};
-		CountingSort ct = new CountingSort();
-		System.out.println();
-		print(ct.sort(nums, nums.length-1, 5));
+		int[] nums = {5,3,2,3,4,1,55,12,51,11,66,25,17,};
+
+		AVL tree = new AVL();
+		for(int i = 0; i<nums.length;i++) tree.insert(nums[i]);
+		
+		tree.print();
 	}
 
 	private static void print(int[] nums)
